e4d05a577754699963fe4043333ab36c4431e241
chiranth <chiranthdv45@gmail.com>
2025-05-14 00:31:48 +0530

    Bind Vite to port 10000

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  server: {
+    port: 10000, // Ensure the app listens on port 10000
+  },
+  build: {
+    outDir: 'dist', // Ensure the build output goes to the 'dist' directory
+  }
 })
